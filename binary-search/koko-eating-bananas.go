54057b1efa1a1e8d84b1d1a08b2642cff53e1af5
Rifaudeen <rifajas@gmail.com>
2025-03-06 15:45:57 +0400

    koko eating bananas

---
+++ binary-search/koko-eating-bananas.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"math"
+	"slices"
+)
+
+func MinEatingSpeed(piles []int, h int) int {
+	left := 1
+	right := slices.Max(piles)
+
+	for left <= right {
+		mid := (left + right) / 2
+		total := countHours(piles, mid)
+
+		if total <= h {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return left
+}
+
+func countHours(piles []int, hourly int) int {
+	total := 0
+
+	for banana := range piles {
+		total += int(math.Ceil(float64(piles[banana]) / float64(hourly)))
+	}
+
+	return total
+}

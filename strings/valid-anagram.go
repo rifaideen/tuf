2382b0588e1f0a63d7001447ff471535167131a3
Rifaudeen <rifajas@gmail.com>
2025-03-29 16:05:37 +0400

    check for valid anagram

--- strings/valid-anagram.go
+++ strings/valid-anagram.go
@@ -3,14 +3,17 @@ package main
 func isAnagram(s string, t string) bool {
 	strMap := map[rune]int{}
 
+	// count the frequency of charecters in s
 	for _, v := range s {
 		strMap[v] += 1
 	}
 
+	// deduct the frequency of charecters in t
 	for _, v := range t {
 		strMap[v] -= 1
 	}
 
+	// check if we have str map is zero
 	for _, v := range strMap {
 		if v != 0 {
 			return false

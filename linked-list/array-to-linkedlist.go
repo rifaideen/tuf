4f2a62ae27565561ccd8af3065b9e72c05b32dba
Rifaudeen <rifajas@gmail.com>
2025-04-04 11:42:45 +0400

    array to linked list

---
+++ linked-list/array-to-linkedlist.go
@@ -0,0 +1,25 @@
+package main
+
+type Node struct {
+	data int
+	next *Node
+}
+
+func constructLL(nums []int) *Node {
+	head := &Node{
+		data: nums[0],
+	}
+
+	current := head
+
+	for i := 1; i < len(nums); i++ {
+		node := &Node{
+			data: nums[i],
+		}
+
+		current.next = node
+		current = node
+	}
+
+	return head
+}

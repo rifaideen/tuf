139c35470fea3df4ebc75fcb8d49be48c69076fe
Rifaudeen <rifajas@gmail.com>
2025-07-21 12:37:39 +0400

    find min and max of binary tree

--- binary-search-tree/min-max-bst/min-max-bst.go
+++ binary-search-tree/min-max-bst/min-max-bst.go
@@ -11,21 +11,58 @@ func findMinMax(root *TreeNode) (int, int) {
 		return -1, -1
 	}
 
+	// recursively find the min and max value of BST
 	return minVal(root, root.Val), maxVal(root, root.Val)
 }
 
+func findMinMaxIterative(root *TreeNode) (int, int) {
+	if root == nil {
+		return -1, -1
+	}
+
+	// start with root value being min and max
+	min := root.Val
+	max := root.Val
+
+	// start the current node from root's left node
+	current := root.Left
+
+	// move all the way to left
+	for current != nil {
+		// update min value to current value and move left
+		min = current.Val
+		current = current.Left
+	}
+
+	// start the current node from root's right node
+	current = root.Right
+
+	// move all the way to right
+	for current != nil {
+		// update min value to current value and move right
+		max = current.Val
+		current = current.Right
+	}
+
+	return min, max
+}
+
 func minVal(root *TreeNode, min int) int {
+	// when root becomes null, whatever is min currently, return it
 	if root == nil {
 		return min
 	}
 
+	// mowe towards left with current root value being the min value
 	return minVal(root.Left, root.Val)
 }
 
-func maxVal(root *TreeNode, min int) int {
+func maxVal(root *TreeNode, max int) int {
+	// when root becomes null, whatever is max currently, return it
 	if root == nil {
-		return min
+		return max
 	}
 
+	// mowe towards right with current root value being the max value
 	return maxVal(root.Right, root.Val)
 }
--- binary-search-tree/min-max-bst/min-max-bst_test.go
+++ binary-search-tree/min-max-bst/min-max-bst_test.go
@@ -79,3 +79,77 @@ func Test_findMinMax(t *testing.T) {
 		})
 	}
 }
+
+func Test_findMinMaxIterative(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		root  *TreeNode
+		want  int
+		want2 int
+	}{
+		{
+			root: &TreeNode{
+				Val: 4,
+				Left: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val: 1,
+					},
+					Right: &TreeNode{
+						Val: 3,
+					},
+				},
+				Right: &TreeNode{
+					Val: 7,
+				},
+			},
+			want:  1,
+			want2: 7,
+		},
+		{
+			root: &TreeNode{
+				Val: 4,
+				Left: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val: 1,
+					},
+					Right: &TreeNode{
+						Val: 3,
+					},
+				},
+				Right: &TreeNode{
+					Val: 7,
+					Left: &TreeNode{
+						Val: 6,
+					},
+					Right: &TreeNode{
+						Val: 10,
+					},
+				},
+			},
+			want:  1,
+			want2: 10,
+		},
+		{
+			root: &TreeNode{
+				Val: 10,
+			},
+			want:  10,
+			want2: 10,
+		},
+		{
+			root:  nil,
+			want:  -1,
+			want2: -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got2 := findMinMaxIterative(tt.root)
+			assert.Equal(t, tt.want, got)
+			assert.Equal(t, tt.want2, got2)
+		})
+	}
+}
